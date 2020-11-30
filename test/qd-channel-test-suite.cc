79f51a48d4c1ee73293339e3d4694e880829a2a8
Mattia Lecci <mattia.lecci@gmail.com>
2020-11-30 11:29:51 +0100

    Update test suite

--- test/qd-channel-test-suite.cc
+++ test/qd-channel-test-suite.cc
@@ -3,33 +3,33 @@
 // Include a header file from your module to test.
 #include "ns3/qd-channel-model.h"
 
-// An essential include is test.h
 #include "ns3/test.h"
+#include "ns3/node-container.h"
+#include "ns3/constant-position-mobility-model.h"
 
 // Do not put your test classes in namespace ns3.  You may find it useful
 // to use the using directive to access the ns3 namespace directly
 using namespace ns3;
 
-// This is an example TestCase.
-class QdChannelTestCase1 : public TestCase
+// Test case for importing information from the Input/ folder
+class QdChannelTestCaseInput : public TestCase
 {
 public:
-  QdChannelTestCase1 ();
-  virtual ~QdChannelTestCase1 ();
+  QdChannelTestCaseInput ();
+  virtual ~QdChannelTestCaseInput ();
 
 private:
   virtual void DoRun (void);
 };
 
-// Add some help text to this case to describe what it is intended to test
-QdChannelTestCase1::QdChannelTestCase1 ()
-  : TestCase ("QdChannel test case (does nothing)")
+QdChannelTestCaseInput::QdChannelTestCaseInput ()
+  : TestCase ("QdChannelTestCaseInput")
 {
 }
 
 // This destructor does nothing but we include it as a reminder that
 // the test case should clean up after itself
-QdChannelTestCase1::~QdChannelTestCase1 ()
+QdChannelTestCaseInput::~QdChannelTestCaseInput ()
 {
 }
 
@@ -38,12 +38,30 @@ QdChannelTestCase1::~QdChannelTestCase1 ()
 // TestCase must implement
 //
 void
-QdChannelTestCase1::DoRun (void)
+QdChannelTestCaseInput::DoRun (void)
 {
-  // A wide variety of test macros are available in src/core/test.h
-  NS_TEST_ASSERT_MSG_EQ (true, true, "true doesn't equal true for some reason");
-  // Use this one for floating point comparisons
-  NS_TEST_ASSERT_MSG_EQ_TOL (0.01, 0.01, 0.001, "Numbers are not equal within tolerance");
+  // Creating nodes
+  // Code breaks (and thus the test fails) if not all positions from
+  // Output/Ns3/NodesPosition/NodesPosition.csv are associated to a node
+  NodeContainer nodes;
+  nodes.Create (2);
+
+  Ptr<MobilityModel> mob0 = CreateObject<ConstantPositionMobilityModel> ();
+  mob0->SetPosition (Vector (5, 0.1, 1.5));
+  Ptr<MobilityModel> mob1 = CreateObject<ConstantPositionMobilityModel> ();
+  mob1->SetPosition (Vector (5, 0.1, 2.9));
+
+  nodes.Get (0)->AggregateObject (mob0);
+  nodes.Get (1)->AggregateObject (mob1);
+
+  // Create the channel model
+  std::string qdFilesPath = "contrib/qd-channel/model/QD/"; // The path of the folder with the QD scenarios
+  std::string scenario = "Indoor1"; // The name of the scenario
+  Ptr<QdChannelModel> qdChannel = CreateObject<QdChannelModel> (qdFilesPath, scenario);
+
+  // tests
+  NS_TEST_ASSERT_MSG_EQ_TOL (qdChannel->GetQdSimTime ().GetSeconds (), 15.665, 1e-9, "Checking simulation time");
+  NS_TEST_ASSERT_MSG_EQ_TOL (qdChannel->GetFrequency (), 60e9, 1, "Checking simulation frequency");
 }
 
 // The TestSuite class names the TestSuite, identifies what type of TestSuite,
@@ -57,12 +75,11 @@ public:
 };
 
 QdChannelTestSuite::QdChannelTestSuite ()
-  : TestSuite ("qd-channel", UNIT)
+  : TestSuite ("qd-channel-test-suite", UNIT)
 {
   // TestDuration for TestCase can be QUICK, EXTENSIVE or TAKES_FOREVER
-  AddTestCase (new QdChannelTestCase1, TestCase::QUICK);
+  AddTestCase (new QdChannelTestCaseInput, TestCase::QUICK);
 }
 
 // Do not forget to allocate an instance of this TestSuite
 static QdChannelTestSuite sqdChannelTestSuite;
-
